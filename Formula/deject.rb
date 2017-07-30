73d739c1c7f47d91a17ab5824598013f9749b97c
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-30 13:37:33 +0900

    deject version v0.1.2

--- Formula/deject.rb
+++ Formula/deject.rb
@@ -1,9 +1,9 @@
 class Deject < Formula
   desc "Deject helps you to manage directories for projects."
   homepage "https://github.com/kyoh86/deject"
-  url "https://github.com/kyoh86/deject/releases/download/v0.1.1/deject_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "1ef645214d17fb4ce1c6f087ccfc8d0bb6ef68eedcfd7d53feff83a5ddd08df5"
+  url "https://github.com/kyoh86/deject/releases/download/v0.1.2/deject_0.1.2_darwin_amd64.tar.gz"
+  version "0.1.2"
+  sha256 "035747e7655b196198bc422743bb0337a6cceca91f8d63ec89e186d4d23dc989"
 
   def install
     bin.install "deject"
