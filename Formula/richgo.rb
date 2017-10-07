797ff016e8d9966fb6f49b2213212976298bf307
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-07 14:43:45 +0900

    richgo version v0.2.5

--- Formula/richgo.rb
+++ Formula/richgo.rb
@@ -1,9 +1,9 @@
 class Richgo < Formula
   desc "Checks for unpinned variables in go programs."
   homepage "https://github.com/kyoh86/richgo"
-  url "https://github.com/kyoh86/richgo/releases/download/v0.2.4/richgo_0.2.4_darwin_amd64.tar.gz"
-  version "0.2.4"
-  sha256 "cf41dae3430bb22a87ed50d02f16486ad80bfb2751ceba985695e60d8ba90b90"
+  url "https://github.com/kyoh86/richgo/releases/download/v0.2.5/richgo_0.2.5_darwin_amd64.tar.gz"
+  version "0.2.5"
+  sha256 "c6e6abec5a333967e763f383aaad83b951404dbae49a4bab998d69229877a4bf"
 
   def install
     bin.install "richgo"
