5ca2957c5dacd8ebbbacc398adc383af642b8c7f
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-06 21:31:13 +0900

    Brew formula update for git-prompt version v0.0.2

--- Formula/git-prompt.rb
+++ Formula/git-prompt.rb
@@ -1,9 +1,9 @@
 class GitPrompt < Formula
   desc "A tool for Prompting git informations"
   homepage "https://github.com/kyoh86/git-prompt"
-  url "https://github.com/kyoh86/git-prompt/releases/download/v0.0.1/'git-prompt_0.0.1_darwin_amd64'.tar.gz"
-  version "0.0.1"
-  sha256 "d75152508ffcc1f13b8a740e1d2d5eba28c63e672c1d1a22beae3798cfe613e9"
+  url "https://github.com/kyoh86/git-prompt/releases/download/v0.0.2/git-prompt_0.0.2_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "3dcef1110614c8a1b00c3d3ec48e295259f39f4ffd31bc1ba566bab64b316016"
 
   def install
     bin.install "git-prompt"
