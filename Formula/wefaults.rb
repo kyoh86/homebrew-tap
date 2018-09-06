b9fed6a4354fdc509e39eecc2ac11161a20fdf3a
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-07 00:21:24 +0900

    Brew formula update for wefaults version v0.0.2

--- Formula/wefaults.rb
+++ Formula/wefaults.rb
@@ -1,9 +1,9 @@
 class Wefaults < Formula
   desc "Watch defaults and show command when they&#39;re changed."
   homepage "https://github.com/kyoh86/wefaults"
-  url "https://github.com/kyoh86/wefaults/releases/download/v0.0.1/'wefaults_0.0.1_darwin_amd64'.tar.gz"
-  version "0.0.1"
-  sha256 "1cc6682ec72f57080b5a96267db5878b7d663295a2b0d13164445eee4bf428c0"
+  url "https://github.com/kyoh86/wefaults/releases/download/v0.0.2/wefaults_0.0.2_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "20cce1c40a75f7ed763dcd1f92c38ed82f120338b2667fd7dda4f5959519ae53"
 
   def install
     bin.install "wefaults"
