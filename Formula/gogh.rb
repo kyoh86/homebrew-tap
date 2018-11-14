e9e276b087223105f936518e42fd623ca5832c91
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-14 22:15:42 +0900

    Brew formula update for gogh version v0.0.4

--- Formula/gogh.rb
+++ Formula/gogh.rb
@@ -3,13 +3,13 @@ class Gogh < Formula
   homepage "https://github.com/kyoh86/gogh"
   url "https://github.com/kyoh86/gogh/releases/download/v0.0.4/gogh_0.0.4_darwin_amd64.tar.gz"
   version "0.0.4"
-  sha256 "db9f2314456307413e062d64eb47367168719bd1f739599588a0be9a06b05f46"
+  sha256 "89c84f9dd1e7ab40df5c9fe123016b39822b64e235f69ab802b2c05d40fcfab8"
   
   depends_on "git"
 
   def install
     bin.install "gogh"
-    man1.install gogh.1
+    man1.install "gogh.1"
   end
 
   def caveats; <<~EOS
