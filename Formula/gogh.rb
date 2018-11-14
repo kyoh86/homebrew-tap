00ef4b0f4f751b750bd7aaab5a85ef1b4d466eea
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-14 20:33:11 +0900

    Brew formula update for gogh version v0.0.2

--- Formula/gogh.rb
+++ Formula/gogh.rb
@@ -1,13 +1,20 @@
 class Gogh < Formula
   desc "GO GitHub project manager"
   homepage "https://github.com/kyoh86/gogh"
-  url "https://github.com/kyoh86/gogh/releases/download/v0.0.1/gogh_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "f97d0c9f8ad71a85d6a820d33daa723c427771efdc6a7c43f9a0ba0951e97f03"
+  url "https://github.com/kyoh86/gogh/releases/download/v0.0.2/gogh_0.0.2_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "59acd38be2d5cf2d2240040df6646e208dfcea216202e11bbc2990fbfd4d19e9"
   
   depends_on "git"
 
   def install
-    bin.install "gogh", "gogogh"
+    bin.install "gogh"
+  end
+
+  def caveats; <<~EOS
+    To use "gogogh" command to change directory into the project,
+    add the following line to your .bashrc / .zshrc
+      eval "$(gogh setup)"
+  EOS
   end
 end
