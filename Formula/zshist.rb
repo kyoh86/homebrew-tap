2bf8e1db37c5b5a1285758dcb13c48f6a674c937
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-02 23:39:29 +0900

    Brew formula update for zshist version v0.0.7

--- Formula/zshist.rb
+++ Formula/zshist.rb
@@ -2,9 +2,18 @@
 class Zshist < Formula
   desc "Encode(metafy) / decode(unmetafy) .zsh_history file"
   homepage "https://github.com/kyoh86/zshist"
-  url "https://github.com/kyoh86/zshist/releases/download/v0.0.6/zshist_0.0.6_darwin_amd64.tar.gz"
-  version "0.0.6"
-  sha256 "4f6be66e583335c43453b391d69b73591f4d95b722e9323638e451b89dc89f18"
+  version "0.0.7"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/kyoh86/zshist/releases/download/v0.0.7/zshist_0.0.7_darwin_amd64.tar.gz"
+    sha256 "9a1999e35438efaf179bd33360080aa75e667ffd436e1c6790fe38a597b30325"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/kyoh86/zshist/releases/download/v0.0.7/zshist_0.0.7_linux_amd64.tar.gz"
+      sha256 "8def7be8b763d972e1754a33a915bcfb2f97d8bce227d043123a17af835fe7b4"
+    end
+  end
 
   def install
     bin.install "zshist"
