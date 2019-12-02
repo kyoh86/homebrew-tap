7fc3eee02e9f1f84f3adb10859c615ad8427cc77
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-02 22:26:12 +0900

    Brew formula update for gigamoji version v0.0.2

--- Formula/gigamoji.rb
+++ Formula/gigamoji.rb
@@ -2,16 +2,16 @@
 class Gigamoji < Formula
   desc "generate emoji (like 👍 in slack) banner which support some bitmap font faces"
   homepage "https://github.com/kyoh86/gigamoji"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kyoh86/gigamoji/releases/download/v0.0.1/gigamoji_0.0.1_darwin_amd64.tar.gz"
-    sha256 "1b52384f10e1fc2a20ba72cec4a36a3f51039bf361a899c5486c4d9a05f578c6"
+    url "https://github.com/kyoh86/gigamoji/releases/download/v0.0.2/gigamoji_0.0.2_darwin_amd64.tar.gz"
+    sha256 "0cd37ad52bd9f28ec03f3215749468061adbf70edb4d89597ad5f03aa50c334d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kyoh86/gigamoji/releases/download/v0.0.1/gigamoji_0.0.1_linux_amd64.tar.gz"
-      sha256 "3e13068102d258b441513654ff9a558299c88dac9be638ad08b74945515974d7"
+      url "https://github.com/kyoh86/gigamoji/releases/download/v0.0.2/gigamoji_0.0.2_linux_amd64.tar.gz"
+      sha256 "3614585de5aa34ec7b38a17a3b57ee021ec2355cc66608afcbbee5474e5992ca"
     end
   end
 
