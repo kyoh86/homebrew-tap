dae43797100bd1cac036b802e07c959c13daa9aa
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-08 09:37:37 +0900

    Brew formula update for gogh version v1

--- Formula/gogh.rb
+++ Formula/gogh.rb
@@ -2,16 +2,16 @@
 class Gogh < Formula
   desc "GO GitHub project manager"
   homepage "https://github.com/kyoh86/gogh"
-  version "1.1.2"
+  version "1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kyoh86/gogh/releases/download/v1.1.2/gogh_1.1.2_darwin_amd64.tar.gz"
-    sha256 "e5904799ba7de274858f2eeaf98b5ac242a331b90e0c569779d3cfe15c75640c"
+    url "https://github.com/kyoh86/gogh/releases/download/v1/gogh_1_darwin_amd64.tar.gz"
+    sha256 "c0fe47452b20322177cbfacc10b9ee076de29cf9c180d33666369783153207d3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kyoh86/gogh/releases/download/v1.1.2/gogh_1.1.2_linux_amd64.tar.gz"
-      sha256 "825a47aee24b13078a48befab18a8fd5aaeca05b22a64cef66e60dccc58b5b81"
+      url "https://github.com/kyoh86/gogh/releases/download/v1/gogh_1_linux_amd64.tar.gz"
+      sha256 "cdd9dfb01549119dae917d3873295fad6f26aece8ba2394266185a9b2b8868fa"
     end
   end
 
