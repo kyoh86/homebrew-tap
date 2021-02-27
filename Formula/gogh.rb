c08398a99dbcb654347f01a776865357edc07c96
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-28 00:07:44 +0900

    Brew formula update for gogh version v2.0.5

--- Formula/gogh.rb
+++ Formula/gogh.rb
@@ -5,24 +5,24 @@
 class Gogh < Formula
   desc "GO GitHub project manager"
   homepage "https://github.com/kyoh86/gogh"
-  version "2.0.4"
+  version "2.0.5"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kyoh86/gogh/releases/download/v2.0.4/gogh_2.0.4_darwin_amd64.tar.gz"
-    sha256 "51a7f6b86d1181e230b5b1e6516f320cba3dea0e97c9b6f25b8ebf6425b0f43a"
+    url "https://github.com/kyoh86/gogh/releases/download/v2.0.5/gogh_2.0.5_darwin_amd64.tar.gz"
+    sha256 "212d33b18a90544692929a384d607f5b8ed7d3b972b45396203a20b87a926ff4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kyoh86/gogh/releases/download/v2.0.4/gogh_2.0.4_linux_amd64.tar.gz"
-    sha256 "ce84df2da6d92f539b665fd174619555e4bd458a74f3c86891cabd3cd938ad6c"
+    url "https://github.com/kyoh86/gogh/releases/download/v2.0.5/gogh_2.0.5_linux_amd64.tar.gz"
+    sha256 "be1b5a4b57cdf957712660372b8dab27ff0897f7681183a4139f0f01c06586a3"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/kyoh86/gogh/releases/download/v2.0.4/gogh_2.0.4_linux_armv6.tar.gz"
-    sha256 "92cabe5e0584084c60059c5734251308f4edc20350879ec739ae11be1198f082"
+    url "https://github.com/kyoh86/gogh/releases/download/v2.0.5/gogh_2.0.5_linux_armv6.tar.gz"
+    sha256 "066eb4fe0c11068ec74baa9c2a4f584002295b9e0ebaeb56b12dd7e5745b911f"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/kyoh86/gogh/releases/download/v2.0.4/gogh_2.0.4_linux_arm64.tar.gz"
-    sha256 "4f5f6bd35974615d0aa716aed1d0ba8a57ff258aa8f42666ea0ec4a3fd1aba02"
+    url "https://github.com/kyoh86/gogh/releases/download/v2.0.5/gogh_2.0.5_linux_arm64.tar.gz"
+    sha256 "bf5e5e4eeefc96fd4db518876b9bbf30c017c3bb6253272f5d05040dc03987cf"
   end
 
   def install
