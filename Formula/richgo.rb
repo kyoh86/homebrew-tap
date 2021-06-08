885ec3bb45c9968e909cd26fb76d95f8ccc0bde9
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-08 10:10:07 +0900

    Brew formula update for richgo version v0.3.9

--- Formula/richgo.rb
+++ Formula/richgo.rb
@@ -5,24 +5,28 @@
 class Richgo < Formula
   desc "Rich-Go will enrich `go test` outputs with text decorations"
   homepage "https://github.com/kyoh86/richgo"
-  version "0.3.8"
+  version "0.3.9"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kyoh86/richgo/releases/download/v0.3.8/richgo_0.3.8_darwin_amd64.tar.gz"
-      sha256 "95f01c60acdd0ebd419f789c5baae42eb2273b3824ef58a8acaa00b394bc2585"
+      url "https://github.com/kyoh86/richgo/releases/download/v0.3.9/richgo_0.3.9_darwin_amd64.tar.gz"
+      sha256 "ab667a5a3db892245d9e006ad453bbee418bdc89cd71a0f9df3fa5079d092f06"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/kyoh86/richgo/releases/download/v0.3.9/richgo_0.3.9_darwin_arm64.tar.gz"
+      sha256 "ec1ccd6089a62f3db1636a5abd1b8df86222231ce9bdfe3a094e2ed95c645d75"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kyoh86/richgo/releases/download/v0.3.8/richgo_0.3.8_linux_amd64.tar.gz"
-      sha256 "a4af68a6422b2cf92614bf29dfb4085518d0b0395c291507ce57e44b364fd13c"
+      url "https://github.com/kyoh86/richgo/releases/download/v0.3.9/richgo_0.3.9_linux_amd64.tar.gz"
+      sha256 "1b85bffe27744c092652df1d4c80d165e8726f7b519dbc5c390acfade563b3c0"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kyoh86/richgo/releases/download/v0.3.8/richgo_0.3.8_linux_arm64.tar.gz"
-      sha256 "d8b6134744f747537ecc86a14002c7be59804396ea7aceb2f2de790ec5f81c42"
+      url "https://github.com/kyoh86/richgo/releases/download/v0.3.9/richgo_0.3.9_linux_arm64.tar.gz"
+      sha256 "492f19f02e3078180baca68a06956424356f8896d1ed7ecc0aa9cdd640e058fc"
     end
   end
 
